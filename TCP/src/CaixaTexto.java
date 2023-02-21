b6a799690b30d5b1e7679f2fb4e46a7417d11fbc
Thiago <thiago2taekwondo@gmail.com>
2023-02-21 15:09:15 -0300

    Text wrapping e Barra lateral

--- TCP/src/CaixaTexto.java
+++ TCP/src/CaixaTexto.java
@@ -1,21 +1,28 @@
 import java.awt.Color;
 import java.awt.Font;
 
-import javax.swing.JTextField;
+import javax.swing.*;
 
-public class CaixaTexto extends JTextField{
+public class CaixaTexto extends JTextArea{
 
 	private static final long serialVersionUID = 1L;
 
-	CaixaTexto(int x, int y, int w, int h){
+	
+	CaixaTexto(){
 		
 		//template da caixa de texto
-		this.setBounds(x,y,w,h);
+		int x = 10;
+		int y = 50;
+		int width = 375;
+		int height = 500;
+		
+		this.setBounds(x,y,width,height);
 		this.setFont(new Font("FreeMono", Font.PLAIN, 20));
 		this.setForeground(Color.white);
 		this.setBackground(Color.black);
 		this.setCaretColor(Color.white);
-		this.setCaretPosition(JTextField.CENTER);
-
+		this.setLineWrap(true);
+		this.setWrapStyleWord(true);
+		
 	}
 }
--- TCP/src/Janela.java
+++ TCP/src/Janela.java
@@ -1,6 +1,5 @@
+//import java.awt.*;
 import javax.swing.*;
-import javax.swing.ImageIcon;
-import java.awt.*;
 
 
 public class Janela extends JFrame{
@@ -10,7 +9,7 @@ public class Janela extends JFrame{
 	Janela() {
 		int winWidth = 600;
 		int winHeight = 600;
-		int margin = 100;
+		//int margin = 100;
 
 		//características da janela principal
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,46 +18,33 @@ public class Janela extends JFrame{
 		this.setSize(winWidth,winHeight);
 		this.setResizable(false);
 			
-		//estanciamento das componentes visuais e devidas correções
-		Label labelTexto = new Label("JMIDI - Texto para Sons",0,10,250,30, 20);
-
-		Label labelInstruc = new Label("",385, 50, 200, 375, 13);
-		labelInstruc.setVerticalAlignment(JLabel.TOP);
-		labelInstruc.setText("<html>Glossário de teclas:<br><br>"
-							+ "+ = dobrar volume<br>"
-							+ "- = dividir volume por 2<br>"
-							+ "R+ = aumenta uma oitava<br>"
-							+ "R- = diminui uma oitava<br>"
-							+ "? = toca nota aleatória<br>"
-							+ "NL = mudar instrumento<br>"
-							+ "; = BPM aleatório<br>"
-							+ "BPM+ = BPM aumenta 80<br>"
-							+ "BACKSPACE = pause<br><br>"
-							+ "Glossário de notas:<br><br>"
-							+ "A/a = nota La<br>"
-							+ "B/b = nota Si<br>"
-							+ "C/c = nota Do<br>"
-							+ "D/d = nota Re<br>"
-							+ "E/e = nota Mi<br>"
-							+ "F/f = nota Fa<br>"
-							+ "G/g = nota Sol</html>");
+		//estanciamento das componentes visuais
+		//labels
+		Label labelTexto = new Label("JMIDI - Texto para Sons");
+		Label labelInstruc = new Label();
 
 		ImageIcon imagem = new ImageIcon("inf.png");
-		Label labelImagem = new Label("",winWidth-125, winWidth-125,100,100, 10);
-
-		CaixaTexto caixaTexto = new CaixaTexto(10,50,375,500);
-			
+		Label labelImagem = new Label(imagem);
+		
+		//campo de texto
+		CaixaTexto caixaTexto = new CaixaTexto();
+		
+		//botão
 		Botão button = new Botão(caixaTexto);
-
+		
+		//barra lateral e correções
+		JScrollPane barraLateral = new JScrollPane(caixaTexto);
+		barraLateral.setBounds(caixaTexto.getBounds());
+		barraLateral.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		
 		//Adição dos componentes visuais à janela
 		this.add(labelImagem);
 		this.add(labelTexto);
 		this.add(labelInstruc);
-		this.add(caixaTexto);
 		this.add(button);
+		this.add(barraLateral);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
-		labelImagem.setIcon(imagem);
 
 	}
 	
