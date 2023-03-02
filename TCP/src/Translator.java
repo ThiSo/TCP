0b7a6c31f38c01e1d77016b0327dbd2e1612787f
Leonardo Azzi Martins <martinsl@dbserver.com.br>
2023-03-01 21:56:48 -0300

    R14: Aumenta até nove oitavas, se não volta ao default

--- TCP/src/Translator.java
+++ TCP/src/Translator.java
@@ -9,36 +9,51 @@ public class Translator {
 
         double MIN_VOL = 0.1D;
         double MAX_VOL = 1.0D;
+        int MIN_OCTAVE = 0;
+        int MAX_OCTAVE = 9;
         double volumePercent = MIN_VOL;
+
+        int octave = MIN_OCTAVE;
 
         for(int i=0; i<text.length(); i++){
             char c = text.charAt(i);
+            int offsetOctave = 12*octave;
 
             switch (c){
                 case ' ':
                     volumePercent = volumePercent * 2;
                     break;
                 case 'C': //DÓ
-                    notas.add(24);
+                    notas.add(12+offsetOctave);
                     break;
                 case 'D': //RÉ;
-                    notas.add(26);
+                    notas.add(14+offsetOctave);
                     break;
                 case 'E': //MI
-                    notas.add(28);
+                    notas.add(16+offsetOctave);
                     break;
                 case 'F': //FA
-                    notas.add(29);
+                    notas.add(17+offsetOctave);
                     break;
                 case 'G': //SOL
-                    notas.add(31);
+                    notas.add(19+offsetOctave);
                     break;
                 case 'A': //LA
-                    notas.add(33);
+                    notas.add(21+offsetOctave);
                     break;
                 case 'B': //SI
-                    notas.add(35);
+                    notas.add(23+offsetOctave);
                     break;
+                case '?':
+                    octave = octave + 1;
+                    break;
+                case '.':
+                    octave = octave + 1;
+                    break;
+            }
+
+            if (octave > MAX_OCTAVE){
+                octave = MIN_OCTAVE;
             }
 
         }
@@ -47,10 +62,12 @@ public class Translator {
             volumePercent = MIN_VOL;
         }
 
-        System.out.println(volumePercent);
-        System.out.println(notas);
-
         int instrumento = 1 ;
+
+        System.out.println("Volume: " + volumePercent);
+        System.out.println("Oitava: " + octave);
+        System.out.println("Notas: " + notas);
+        System.out.println("Instrumento: " + instrumento);
 
         new MidiPlayer(notas, instrumento, volumePercent);
     }
